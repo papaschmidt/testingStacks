0bf1ad28b077084268921f0c0a6fea6b25c8976f
Matthew Schmidt <mschmidt@papa.com>
2023-12-15 12:02:36 -0600

    initial commit
    Created using spr 1.3.5

--- App.tsx
+++ App.tsx
@@ -69,10 +69,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
+          <Section title="Step One">Take a break and relax man</Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
